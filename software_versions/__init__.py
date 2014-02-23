ff1703e48ebe8f5869e507039e4aa4f40c20ea80
Vedran Krivokuca <vkrivokuca+github@gmail.com>
2014-02-23 08:10:44 +0100

    Dual parsing system set up

--- software_versions/__init__.py
+++ software_versions/__init__.py
@@ -5,93 +5,65 @@
 latest software versions from respective vendors' web sites.
 '''
 
-import software_versions.re as sre
-import software_versions.dom as sdom
+import software_versions.reparser as sre
+import software_versions.domparser as sdom
 
-STABLE_SENDMAIL_URL = "http://www.sendmail.com/sm/open_source/download/"
-STABLE_SENDMAIL_PATTERN = \
-    r'Sendmail.*<a href="/sm/open_source/download/.*">(.*)</a> is ' \
-    r'available from'
+STABLE_SENDMAIL_URL = sre.STABLE_SENDMAIL_URL
+STABLE_SENDMAIL_PATTERN = sre.STABLE_SENDMAIL_PATTERN
 
-STABLE_BIND_URL = \
-    "https://www.isc.org/downloads/"
-STABLE_BIND_PATTERN = \
-    r'<tr>\n<td>(\d+.*)</td>\n<td class="c_sw"><a href'
+STABLE_BIND_URL = sre.STABLE_BIND_URL
+STABLE_BIND_PATTERN = sre.STABLE_BIND_PATTERN
 
-STABLE_DHCP_URL = \
-    "https://www.isc.org/downloads/"
-STABLE_DHCP_PATTERN = \
-    r'<tr>\n<td><span.*>(\d+.*)</span></td>\n<td class="c_sw"'
+STABLE_DHCP_URL = sre.STABLE_DHCP_URL
+STABLE_DHCP_PATTERN = sre.STABLE_DHCP_PATTERN
 
-STABLE_CYRUS_IMAP_URL = "http://www.cyrusimap.org/"
-STABLE_CYRUS_IMAP_PATTERN = \
-    r'<p>Version: (.*?)</p>'
+STABLE_CYRUS_IMAP_URL = sre.STABLE_CYRUS_IMAP_URL
+STABLE_CYRUS_IMAP_PATTERN = sre.STABLE_CYRUS_IMAP_PATTERN
 
-STABLE_APACHE_URL = "http://httpd.apache.org/download.cgi"
-STABLE_APACHE_PATTERN = \
-    r'<a href="\#apache.*">(.*?)</a> \(released'
+STABLE_APACHE_URL = sre.STABLE_APACHE_URL
+STABLE_APACHE_PATTERN = sre.STABLE_APACHE_PATTERN
 
-STABLE_SQUID_URL = "http://www.squid-cache.org/Versions/"
-STABLE_SQUID_PATTERN = \
-    r'<tr><td><a href="v.*?">.*?</a></td><td>.*?</td><td>(.*?)</td></tr>'
+STABLE_SQUID_URL = sre.STABLE_SQUID_URL
+STABLE_SQUID_PATTERN = sre.STABLE_SQUID_PATTERN
 
-STABLE_CANIT_URL = "http://roaringpenguin.com/news"
-STABLE_CANIT_PATTERN = \
-    r'<a href.*?>CanIt (.*?) '
+STABLE_CANIT_URL = sre.STABLE_CANIT_URL
+STABLE_CANIT_PATTERN = sre.STABLE_CANIT_PATTERN
 
-STABLE_PSQL_URL = "http://www.postgresql.org/versions.rss"
-STABLE_PSQL_PATTERN = \
-    r'<item><title>(.*?)(\<\/|$)'
+STABLE_PSQL_URL = sre.STABLE_PSQL_URL
+STABLE_PSQL_PATTERN = sre.STABLE_PSQL_PATTERN
 
-STABLE_SQUIRRELMAIL_URL = "http://squirrelmail.org/download.php"
-STABLE_SQUIRRELMAIL_PATTERN = \
-    r'<b>Stable version</b>.*?<b>Version: (.*?)</b><br />'
+STABLE_SQUIRRELMAIL_URL = sre.STABLE_SQUIRRELMAIL_URL
+STABLE_SQUIRRELMAIL_PATTERN = sre.STABLE_SQUIRRELMAIL_PATTERN
 
-STABLE_OPENLDAP_URL = "http://www.openldap.org/software/download/"
-STABLE_OPENLDAP_PATTERN = \
-    r'Our latest release.*?of OpenLDAP Software for general use' \
-    r'.*?OpenLDAP-(.*?) is currently available'
+STABLE_OPENLDAP_URL = sre.STABLE_OPENLDAP_URL
+STABLE_OPENLDAP_PATTERN = sre.STABLE_OPENLDAP_PATTERN
 
-STABLE_PLA_URL = "http://phpldapadmin.sourceforge.net/wiki/index.php/Release"
-STABLE_PLA_PATTERN = \
-    r'<span class="mw-headline" id="phpLDAPadmin_.*?">' \
-    r'phpLDAPadmin (.*?)</span></h1>'
+STABLE_PLA_URL = sre.STABLE_PLA_URL
+STABLE_PLA_PATTERN = sre.STABLE_PLA_PATTERN
 
-STABLE_POSTFIX_URL = "http://de.postfix.org/ftpmirror/"
-STABLE_POSTFIX_PATTERN = \
-    r'<a href="official/postfix-(.*).tar.gz">Source code</a>'
+STABLE_POSTFIX_URL = sre.STABLE_POSTFIX_URL
+STABLE_POSTFIX_PATTERN = sre.STABLE_POSTFIX_PATTERN
 
-STABLE_NGINX_URL = "http://nginx.org/en/download.html"
-STABLE_NGINX_PATTERN = \
-    r'Stable version</h4>.*?<a href="/download/nginx-(.*?).tar.gz">'
+STABLE_NGINX_URL = sre.STABLE_NGINX_URL
+STABLE_NGINX_PATTERN = sre.STABLE_NGINX_PATTERN
 
-STABLE_MYSQL_URL = "http://www.mysql.com/downloads/mysql/"
-STABLE_MYSQL_PATTERN = \
-    r'<h1>MySQL Community Server (.*?)</h1>'
+STABLE_MYSQL_URL = sre.STABLE_MYSQL_URL
+STABLE_MYSQL_PATTERN = sre.STABLE_MYSQL_PATTERN
 
-STABLE_PROFTPD_URL = "http://www.proftpd.org/"
-STABLE_PROFTPD_PATTERN = \
-    r'Stable: <strong>(.*?)</strong>'
+STABLE_PROFTPD_URL = sre.STABLE_PROFTPD_URL
+STABLE_PROFTPD_PATTERN = sre.STABLE_PROFTPD_PATTERN
 
-STABLE_VSFTPD_URL = "https://security.appspot.com/vsftpd.html"
-STABLE_VSFTPD_PATTERN = \
-    r'<li>vsftpd-(.*?) is released'
+STABLE_VSFTPD_URL = sre.STABLE_VSFTPD_URL
+STABLE_VSFTPD_PATTERN = sre.STABLE_VSFTPD_PATTERN
 
-STABLE_SENDMAILANALYZER_URL = \
-    "http://sourceforge.net/projects/sa-report/files/"
-STABLE_SENDMAILANALYZER_PATTERN = \
-    r'<span>Download sendmailanalyzer-(.*?).tar.gz '
+STABLE_SENDMAILANALYZER_URL = sre.STABLE_SENDMAILANALYZER_URL
+STABLE_SENDMAILANALYZER_PATTERN = sre.STABLE_SENDMAILANALYZER_PATTERN
 
-STABLE_DJANGO_URL = "https://www.djangoproject.com/download/"
-STABLE_DJANGO_PATTERN = \
-    r'<p>The latest official version is (.*?).</p>'
+STABLE_DJANGO_URL = sre.STABLE_DJANGO_URL
+STABLE_DJANGO_PATTERN = sre.STABLE_DJANGO_PATTERN
 
-STABLE_HPBIOS_URL = \
-    "http://forum.notebookreview.com/hp-business-class-notebooks/" \
-    "601489-current-hp-business-bios-releases.html"
-STABLE_HPBIOS_PATTERN = \
-    '<br>8560p</font></strong></td><td style="border: 1px solid #000000; ' \
-    'width: 75px;" valign="top">(.*?)</td>'
+STABLE_HPBIOS_URL = sre.STABLE_HPBIOS_URL
+STABLE_HPBIOS_PATTERN = sre.STABLE_HPBIOS_PATTERN
 
 __all__ = [
     "STABLE_SENDMAIL_URL", "STABLE_SENDMAIL_PATTERN",
--- software_versions/versions.py
+++ software_versions/versions.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 '''
 Created on Nov 21, 2011
 
