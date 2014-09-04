af0028258fe63f187dd0749ed3c18f48cc862b7c
Igor Vuk <parcijala@gmail.com>
2014-09-05 00:06:37 +0200

    Add Pound test to versions_test.py

--- unittest/versions_test.py
+++ unittest/versions_test.py
@@ -114,6 +114,11 @@ class TestVersionFunctions(unittest.TestCase):
             self.__test_version(ver.check_stunnel()) \
         )
 
+    def test_pound(self):
+        self.assertTrue( \
+            self.__test_version(ver.check_pound()) \
+        )
+
     def __test_version(self, input_values=None):
 
         if (input_values):
