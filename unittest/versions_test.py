5d4df732625e52d459c2a5204d90384aba96248a
Igor Vuk <parcijala@gmail.com>
2014-10-02 21:45:22 +0200

    Add OpenSMTPD test to versions_test.py

--- unittest/versions_test.py
+++ unittest/versions_test.py
@@ -214,6 +214,11 @@ class TestVersionFunctions(unittest.TestCase):
             self.__test_version(ver.check_owncloud()) \
         )
 
+    def test_opensmtpd(self):
+        self.assertTrue( \
+            self.__test_version(ver.check_opensmtpd()) \
+        )
+
     def __test_version(self, input_values=None):
 
         if (input_values):
