38c1d572455b89149572904324f4cab7510e54df
Igor Vuk <parcijala@gmail.com>
2016-06-13 18:49:38 +0200

    Fix Bacula version parsing

--- software_versions/__init__.py
+++ software_versions/__init__.py
@@ -158,9 +158,9 @@ STABLE_WORDPRESS_PATTERN = \
     r'<p class="intro">The latest stable release of WordPress ' \
     r'\(Version (.*?)\)'
 
-STABLE_BACULA_URL = "http://sourceforge.net/projects/bacula/files/bacula/"
+STABLE_BACULA_URL = "https://sourceforge.net/projects/bacula/files/bacula/"
 STABLE_BACULA_PATTERN = \
-    r'<div id="files"><div class="download-bar">Looking for the latest ' \
+    r'<div class="download-bar">Looking for the latest ' \
     r'version\? <strong>.*?' \
     r'<a href="/projects/bacula/files/latest/download\?source=files" ' \
     r'title="/bacula/.*?/bacula-(.*?).tar.gz'
